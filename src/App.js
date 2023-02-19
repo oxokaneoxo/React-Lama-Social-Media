e2b90576a886f90c02d0ad513b104e00d8af45ce
oxokaneoxo <kane.miller@hotmail.com>
2023-02-19 14:55:25 -0800

    Set up dark/light mode. using UseContext/useEffect

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import LeftBar from "./components/leftBar/LeftBar"
 import RightBar from "./components/rightBar/RightBar"
 import Home from "./pages/home/Home"
 import Profile from "./pages/profile/Profile"
+import "./style.scss"
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 
 
 function App() {
+   const currentUser = true;
 
-  const currentUser = true;
+  const {darkMode} = useContext(DarkModeContext);
+
 
   const  Layout = () => {
     return(
-      <div>
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <Navbar />
         <div style={{display: 'flex'}}>
           <LeftBar />
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import "./home.scss"
 const Home = () => {
   return (
-    <div className="Home">
+    <div className="home">
       <h1>Home</h1>
       <h1>Home</h1>
       <h1>Home</h1>
