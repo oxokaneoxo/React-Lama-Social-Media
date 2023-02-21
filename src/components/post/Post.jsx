eb4e0f0312406081b96dbfb8c0dcfdb2d73f1389
oxokaneoxo <kane.miller@hotmail.com>
2023-02-20 16:29:05 -0800

    Ready for the Backend

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,9 +5,13 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
+import Comments from "../comments/Comments";
+import { useState } from "react";
 
 
 const Posts = ({ post }) => {
+
+  const [commentOpen, setCommentOpen] = useState(false);
 
   //temportary
   const liked = false;
@@ -37,15 +41,16 @@ const Posts = ({ post }) => {
             {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
             12 likes
           </div>
-          <div className="item">
+          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
-            12 Comments
+            2 Comments
           </div>
           <div className="item">
             <ShareOutlinedIcon />
             Share
           </div>
         </div>
+        {commentOpen && <Comments />}
       </div>
     </div>
   )
